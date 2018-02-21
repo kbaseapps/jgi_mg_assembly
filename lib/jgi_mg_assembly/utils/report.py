7cddb33ca7e9089f95136c5cb600f614ecdb49a4
Bill Riehl <wjriehl@lbl.gov>
2018-02-20 19:23:06 -0800

    report changes.

--- lib/jgi_mg_assembly/utils/report.py
+++ lib/jgi_mg_assembly/utils/report.py
@@ -5,6 +5,7 @@ import os
 import errno
 import uuid
 import shutil
+import zipfile
 from DataFileUtil.DataFileUtilClient import DataFileUtil
 from KBaseReport.KBaseReportClient import KBaseReport
 
@@ -23,29 +24,44 @@ class ReportUtil(object):
             "description": some string
         }
         """
-        report_dir = os.path.join(self.scratch_dir, "report_{}".format(uuid.uuid4()))
-        self._mkdir_p(report_dir)
+        html_report_dir = os.path.join(self.scratch_dir, "report_{}".format(uuid.uuid4()))
+        self._mkdir_p(html_report_dir)
 
         stats_file_name = os.path.basename(stats_file)
         coverage_file_name = os.path.basename(coverage_file)
-        shutil.copy(stats_file, os.path.join(report_dir, stats_file_name))
-        shutil.copy(coverage_file, os.path.join(report_dir, coverage_file_name))
+        result_file = os.path.join(html_report_dir, 'assembly_report.zip')
+        with zipfile.ZipFile(result_file, 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as report_zip:
+            report_zip.write(stats_file, stats_file_name)
+            report_zip.write(coverage_file, coverage_file_name)
+        file_links = [{
+            'path': result_file,
+            'name': os.path.basename(result_file),
+            'label': 'assembly_report',
+            'description': 'JGI Metagenome Assembly Report'
+        }]
+        shutil.copy(stats_file, os.path.join(html_report_dir, stats_file_name))
+        shutil.copy(coverage_file, os.path.join(html_report_dir, coverage_file_name))
 
-        html_file_name = os.path.join(report_dir, "index.html")
+        html_file_name = os.path.join(html_report_dir, "index.html")
         self._write_html_file(html_file_name, stats_file, coverage_file)
-        return self._upload_report(report_dir, workspace_name, saved_objects)
+        return self._upload_report(html_report_dir, file_links, workspace_name, saved_objects)
 
     def _write_html_file(self, html_file_name, stats_file, coverage_file):
-        html_content = """
-        <html>
-        <body>
-        test report
-        </body>
-        </html>"""
+        table = ""
+        with open(stats_file, "r") as stats:
+            lines = stats.readlines()
+            head = lines.pop(0).strip().split("\t")
+            table = "<tr><th>{}</th></tr>".format("</th><th>".join(head))
+            for line in lines:
+                elems = "</td><td>".join(line.strip().split("\t"))
+                elems = "<tr><td>{}</td></tr>\n".format(elems)
+                table = table + elems
+        table = "<table>\n{}</table>".format(table)
+        html_content = "<html>\n<body>\n{}\n</body></html>".format(table)
         with open(html_file_name, "w") as outfile:
             outfile.write(html_content)
 
-    def _upload_report(self, report_dir, workspace_name, saved_objects):
+    def _upload_report(self, report_dir, file_links, workspace_name, saved_objects):
         dfu = DataFileUtil(self.callback_url)
         upload_info = dfu.file_to_shock({
             'file_path': report_dir,
@@ -61,6 +77,7 @@ class ReportUtil(object):
                 'name': 'index.html',
                 'description': 'assembly report'
             }],
+            'file_links': file_links,
             'report_object_name': 'JGI_assembly_pipeline.' + str(uuid.uuid4()),
             'workspace_name': workspace_name,
             'objects_created': saved_objects
