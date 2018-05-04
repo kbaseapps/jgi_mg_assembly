f6d6f198e234d31917b7de3383af99ce1073d0f1
Bill Riehl <wjriehl@lbl.gov>
2018-05-04 10:28:54 -0700

    update BBTools client, use dev version of BBTools now (for testing)

--- lib/BBTools/BBToolsClient.py
+++ lib/BBTools/BBToolsClient.py
@@ -51,9 +51,9 @@ class BBTools(object):
            against the Workspace. This doesn't cover all of the 110+
            parameters provided by rqcfilter. Those not listed here are left
            as default values, except sketch=f (as that sends data to JGI
-           servers for processing). Notes below are taken from the help
-           output from rqcfilter.sh ver 37.90 Parameters (format = param name
-           - default - description):
+           servers for processing), barcodefilter=f, and mapk=13. Notes below
+           are taken from the help output from rqcfilter2.sh ver 38.00
+           Parameters (format = param name - default - description):
            --------------------------------------------------------- library
            - frag - should be one of 'frag', 'clip', 'lfpe', or 'clrs'.
            Adapter trimming parameters: ----------------------------
@@ -139,9 +139,9 @@ class BBTools(object):
            against the Workspace. This doesn't cover all of the 110+
            parameters provided by rqcfilter. Those not listed here are left
            as default values, except sketch=f (as that sends data to JGI
-           servers for processing). Notes below are taken from the help
-           output from rqcfilter.sh ver 37.90 Parameters (format = param name
-           - default - description):
+           servers for processing), barcodefilter=f, and mapk=13. Notes below
+           are taken from the help output from rqcfilter2.sh ver 38.00
+           Parameters (format = param name - default - description):
            --------------------------------------------------------- library
            - frag - should be one of 'frag', 'clip', 'lfpe', or 'clrs'.
            Adapter trimming parameters: ----------------------------
